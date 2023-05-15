d5850da51b1e6aadb05fa1cde82f6ad03c1c4f39
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-05-15 20:51:19 +0700

    attempted nav side bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Navigation from './components/Navigation'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Navigation></Navigation>
     </div>
   );
 }
