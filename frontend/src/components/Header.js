0ef29184710a426ede9bca7bdf9cbfad4ec22366
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-05-15 19:00:27 +0700

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import logo from '../icons/logo.png'
+import logout from '../icons/log-out.png'
+import '../App.css'
 
 function Header() {
   return (
     <div>
-        <h1>fitflow</h1>
+      <Box 
+        sx={{
+          height: 40,
+          display: 'flex',
+          alignItems: "center"
+        }}
+        p={2}
+      >
+
+        <div>
+          <img 
+            src={logo}
+            height={40}
+          ></img>
+        </div>
+
+        <div class="right">
+          <img 
+            src={logout}
+            height={40}
+          ></img>
+        </div>
+        
+      </Box>
     </div>
+    
   )
 }
 
