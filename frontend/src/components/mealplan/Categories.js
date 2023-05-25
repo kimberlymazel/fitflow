dcdf6ff1e5d6edac2b3a352863c18923657c6a96
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-05-26 00:08:01 +0700

    worked on meal plan

--- frontend/src/components/mealplan/Categories.js
+++ frontend/src/components/mealplan/Categories.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import * as React from 'react'
 import Small from '../cards/Small'
 import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
 import '../../style/mealplan.css'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 
 import anything from '../../icons/anything.png'
 import paleo from '../../icons/paleo.png'
@@ -11,71 +12,85 @@ import vegan from '../../icons/vegan.png'
 import keto from '../../icons/keto.png'
 
 function Categories() {
-  return (
-    <div className="categories">
-        <Box>
-            <Grid
-                container spacing={1.5}
-                wrap="nowrap"
+    const [selectedDiet, setSelectedDiet] = React.useState('anything');
+    
+    const handleOptionChange = (event, newSelect) => {
+        setSelectedDiet(newSelect);
+    };
+
+    return (
+        <div className="categories">
+            <h1>Create a meal plan</h1>
+            <h3>Reach your diet and nutritional goals
+                with our meal plan generator</h3>
+            <Box
+                textAlign='center'
             >
-                <Grid item>
-                    <Small
-                        style={{
-                            background: '#B497ED'
-                        }}
-                    >
-                        <img src={anything} height={65}></img>
-                        <h5>Anything</h5>
-                    </Small>
-                </Grid>
+                <ToggleButtonGroup
+                    value={selectedDiet}
+                    exclusive
+                    onChange={handleOptionChange}
+                    className="toggle"
+                >
+                    <ToggleButton value="anything">
+                        <Small
+                            style={{
+                                background: '#B497ED'
+                            }}
+                        >
+                            <img src={anything} height={65}></img>
+                            <h5>Anything</h5>
+                        </Small>
+                    </ToggleButton>
 
-                <Grid item>
-                    <Small
-                        style={{
-                            background: '#A781F0'
-                        }}
-                    >
-                        <img src={paleo} height={65}></img>
-                        <h5>Paleo</h5>
-                    </Small>
-                </Grid>
 
-                <Grid item>
-                    <Small
-                        style={{
-                            background: '#996CF0'
-                        }}
-                    >
-                        <img src={vegetarian} height={65}></img>
-                        <h5>Vegetarian</h5>
-                    </Small>
-                </Grid>
+                    <ToggleButton value="paleo">
+                        <Small
+                            style={{
+                                background: '#A781F0'
+                            }}
+                        >
+                            <img src={paleo} height={65}></img>
+                            <h5>Paleo</h5>
+                        </Small>
+                    </ToggleButton>
 
-                <Grid item>
-                    <Small
-                        style={{
-                            background: '#8D5AF0'
-                        }}
-                    >
-                        <img src={vegan} height={65}></img>
-                        <h5>Vegan</h5>
-                    </Small>
-                </Grid>
+                    <ToggleButton value="vegetarian">
+                        <Small
+                            style={{
+                                background: '#996CF0'
+                            }}
+                        >
+                            <img src={vegetarian} height={65}></img>
+                            <h5>Vegetarian</h5>
+                        </Small>
+                    </ToggleButton>
 
-                <Grid item>
-                    <Small
-                        style={{
-                            background: '#7E44EE'
-                        }}
-                    >
-                        <img src={keto} height={65}></img>
-                        <h5>Keto</h5>
-                    </Small>
-                </Grid>
-            </Grid>
-        </Box>
-    </div>
-  )
+                    <ToggleButton value="vegan"> 
+                        <Small
+                            style={{
+                                background: '#8D5AF0'
+                            }}
+                        >
+                            <img src={vegan} height={65}></img>
+                            <h5>Vegan</h5>
+                        </Small>
+                    </ToggleButton>
+
+                    <ToggleButton value="keto">
+                        <Small
+                            style={{
+                                background: '#7E44EE'
+                            }}
+                        >
+                            <img src={keto} height={65}></img>
+                            <h5>Keto</h5>
+                        </Small>
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </Box>
+        </div>
+    ) 
 }
 
 export default Categories
--- frontend/src/pages/MealPlanGenerator.js
+++ frontend/src/pages/MealPlanGenerator.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import '../style/App.css'
+import '../style/mealplan.css'
 import Header from '../components/general/Header'
 import Navigation from '../components/general/Navigation'
 import Categories from '../components/mealplan/Categories'
+import DropDown from '../components/mealplan/DropDown'
 
 export const MealPlanGenerator = () => {
     return(
-        <div className="MealPlan">
+        <div className="mealplan">
             <Header></Header>
 
             <div class="left">
                 <Navigation></Navigation>
             </div>
 
-            <div class="middle">
+            <div class="right">
                 <Categories></Categories>
+                <DropDown></DropDown>
             </div>
         </div>
     )
