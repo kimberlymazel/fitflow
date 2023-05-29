0e1098be4b45ceff6afc06f59ee399613dbf9567
mariaclarin <maria.clarin@binus.ac.id>
2023-05-29 11:39:47 +0700

    cleaned and seperated css for auth components

--- frontend/src/components/auth/Info.js
+++ frontend/src/components/auth/Info.js
@@ -1,41 +1,50 @@
 import React from 'react'
 import logoright from '../../icons/logoright.svg'
 import halfcircle from '../../icons/halfcircle.svg'
+import '../../style/auth/info.css'
 
-// , backgroundImage:`url(${infobackground})`
 export const Info = () => {
   return (
-    <div className='info' style={{position: 'relative', overflowX:"hidden", overflowY:"hidden",background:'linear-gradient(to bottom right, #E85959, #8A07C8)', height:"110vh", maxWidth:"100%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", margin:"-5% 0% 0% 0%"}}>
-        <div style={{marginTop:"15%", overflowX:"hidden", overflowY:"hidden"}}>
+    <div className='info'>
+        {/*=========UI Background decor==========*/}
+        <div className="halfcircledecoinfo">
             <img src={halfcircle}></img>
         </div>
-        <div className='infobox' style={{overflow:"hidden",marginTop:"-50%",backgroundColor:"white", padding:"20px 20px 20px 20px", width:"30%", boxShadow:"2px 2px 2px 2px #434343",borderRadius:"30px"}}>
-            <h2 style={{color: '#761BA0', fontSize:'170%', marginLeft:"12%" ,marginTop:'5%'}}>You're almost ready!</h2> 
-            <h2 style={{color: '#761BA0', fontSize:'350%', marginLeft:"12%", fontWeight:"1000",marginTop:'-18px'}}>Fill in your data:</h2> 
-            <div className='weight' style={{display:"flex",marginTop:'-20px'}}>
-                <h2 style={{color: '#761BA0', fontSize:'150%', marginLeft:"12%" }}>Weight:</h2> 
-                <input placeholder=' Enter weight (kg)' style={{fontSize:"20px",height:"30px" , marginTop:"auto", marginBottom:"auto", width:"310px",marginLeft:"10px",borderColor:"#AAAAAA",borderRadius:"5px"}}></input>
+
+
+        {/*==========Personal Info section==========*/}
+        <div className='infobox'>
+            <h2 className='infoboxtitledesc'>You're almost ready!</h2> 
+            <h2 className="infoboxtitle">Fill in your data:</h2> 
+            {/*========Weight Input=========*/}
+            <div className='indivinfobox'>
+                <h2 className="personalinputtitle">Weight:</h2> 
+                <input className='weightinfoinput' placeholder=' Enter weight (kg)'></input>
             </div>
-            <div className='height' style={{display:"flex",marginTop:'-20px'}}>
-                <h2 style={{color: '#761BA0', fontSize:'150%', marginLeft:"12%" }}>Height:</h2> 
-                <input placeholder=' Enter height (cm)' style={{fontSize:"20px",height:"30px" , marginTop:"auto", marginBottom:"auto", width:"310px",marginLeft:"15px",borderColor:"#AAAAAA",borderRadius:"5px"}}></input>
+            {/*========Height Input=========*/}
+            <div className='indivinfobox'>
+                <h2 className="personalinputtitle">Height:</h2> 
+                <input className="heightinfoinput" placeholder=' Enter height (cm)'></input>
             </div>
-            <div className='cardiogoal' style={{display:"flex",marginTop:'-20px'}}>
-                <h2 style={{color: '#761BA0', fontSize:'150%', marginLeft:"12%" }}>Cardio Goal:</h2> 
-                <input placeholder=' Steps' style={{fontSize:"20px",height:"30px" , marginTop:"auto", marginBottom:"auto", width:"252px",marginLeft:"15px",borderColor:"#AAAAAA",borderRadius:"5px"}}></input>
+            {/*========Cardio Goal Input=========*/}
+            <div className='indivinfobox'>
+                <h2 className="personalinputtitle">Cardio Goal:</h2> 
+                <input className="cardiogoalinfoinput" placeholder=' Steps'></input>
             </div>
-            <div className='caloriegoal' style={{display:"flex",marginTop:'-20px'}}>
-                <h2 style={{color: '#761BA0', fontSize:'150%', marginLeft:"12%" }}>Calorie Goal:</h2> 
-                <input placeholder=' Calories to burn' style={{fontSize:"20px",height:"30px" , marginTop:"auto", marginBottom:"auto", width:"252px",marginLeft:"10px",borderColor:"#AAAAAA",borderRadius:"5px"}}></input>
+            {/*========Calorie Goal Input=========*/}
+            <div className='indivinfobox'>
+                <h2 className="personalinputtitle">Calorie Goal:</h2> 
+                <input className="caloriegoalinfoinput" placeholder=' Calories to burn'></input>
             </div>
-            <button  style={{width:"420px",fontSize:"20px", marginLeft: '11%', marginRight:"auto", background:"#971BC2",marginBottom:"20px", color:"white",fontWeight:"600", borderColor:"lightgrey",borderRadius:"15px", boxShadow:"2px 3px 2px 2px lightgrey", padding:"10px 5px 10px 5px"}}>Save and Finish</button>
-            <div style={{marginTop:"-20px", marginBottom:"-30px"}}>
+            {/*========Save Button===============*/}
+            <button className="saveinfobtn">Save and Finish</button>
+
+
+            {/*=========UI Background decor==========*/}
+            <div className="logodeco">
                 <img src={logoright} width={"500px"}></img>
             </div>
         </div>
-
-
-
     </div>
   )
 }
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../services/axios";
 import loginillustration from '../../icons/loginillustration.svg'
 import logo from '../../icons/logo.png'
+import '../../style/auth/register.css'
 
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -33,27 +34,31 @@ export const Register = () => {
       };
 
     return (
-      <div className="register" style={{height:"100vh", display:"flex"}}>
-        <div className="left"  style={{height:"100%", width:"100%", backgroundColor:"white", padding:"40px 40px 0px 0px", marginLeft:"40px"}}>
+      <div className="register">
+        <div className="leftregister">
           <img src={logo} height={60}></img>
-          <div className="regiscontainer" style={{margin:"70px 20px 20px 20px", display:"flex", justifyContent:"center", alignItems:"center"}}>
+          <div className="regiscontainer">
             <div className="regisbox">
-              <h3 style={{color:"#531EB7", fontSize:"250%", fontWeight:900}}>Sign up</h3>
-              <h3 style={{color:"#531EB7", fontSize:"140%", fontWeight:600, marginTop:"-25px", marginBottom:"9px"}}>Email</h3>
+              <h3 className="regisboxtitle">Sign up</h3>
+
+              {/*==========Email Regis===========*/}
+              <h3 className="emailregisinputtitle">Email</h3>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className={errors.email ? 'form-control error' : 'form-control'} style={{display:"flex", flexDirection:"column"}}>
                   <input
+                    className="emailinputfieldregis"
                     type="email"
                     placeholder="Enter email address" 
                     {...register('email', { required: '*This is a required field' })}
-                    style={{width:"400px", fontSize:"20px", borderRadius:"5px", borderColor:"#AAAAAA", padding:"5px 5px 5px 5px", marginBottom:"6px"}}
                   />
                   {errors.email && <span className="error-message" style={{color:"#D30000", fontWeight:600,fontSize:"16px"}}>{errors.email.message}</span>}
                 </div>
 
-                <h3 style={{color:"#531EB7", fontSize:"140%", fontWeight:600, marginTop:"10px", marginBottom:"9px"}}>Username</h3>
+                {/*==========Username Regis===========*/}
+                <h3 className="usernameregisinputtitle">Username</h3>
                 <div className={errors.username ? 'form-control error' : 'form-control'} style={{display:"flex", flexDirection:"column"}}>
                   <input
+                    className="usernameinputfieldregis"
                     type="text"
                     placeholder="Enter username"
                     {...register('username', {
@@ -67,14 +72,15 @@ export const Register = () => {
                         message: 'Username must be at most 24 characters',
                       },
                     })}
-                    style={{width:"400px", fontSize:"20px", borderRadius:"5px", borderColor:"#AAAAAA", padding:"5px 5px 5px 5px", margin:"0px 0px 6px 0px"}}
                   />
                   {errors.username && <span className="error-message" style={{color:"#D30000", fontWeight:600,fontSize:"16px"}}>{errors.username.message}</span>}
                 </div>
 
-                <h3 style={{color:"#531EB7", fontSize:"140%", fontWeight:600, marginTop:"10px", marginBottom:"9px"}}>Password</h3>
+                {/*==========Password Regis===========*/}
+                <h3 className="passwordregisinputtitle">Password</h3>
                 <div className={errors.password ? 'form-control error' : 'form-control'} style={{display:"flex", flexDirection:"column"}}>
                   <input
+                    className="passwordinputfieldregis"
                     type="password"
                     placeholder="Enter password"
                     {...register('password', {
@@ -88,33 +94,31 @@ export const Register = () => {
                         message: 'Password must be at most 24 characters',
                       },
                     })}
-                    style={{width:"400px", fontSize:"20px", borderRadius:"5px", borderColor:"#AAAAAA", padding:"5px 5px 5px 5px", margin:"0px 0px 6px 0px"}}
                   />
                   {errors.password && <span className="error-message" style={{color:"#D30000", fontWeight:600,fontSize:"16px"}}>{errors.password.message}</span>}
                 </div>
-                
-                <button type="submit" disabled={isSubmitting} style={{width:"100%",fontSize:"20px", background:"#531EB7", color:"white", border:"white",borderRadius:"5px", margin:"20px 0px 10px 0px",boxShadow:"2px 2px 2px 2px lightgrey", padding:"10px 5px 10px 5px"}}>
+
+                {/*========BUTTONS===========*/}
+                <button type="submit" disabled={isSubmitting} className="registerbtnregis">
                   {isSubmitting ? 'Creating account...' : 'Register'}
                 </button>
               </form>
-              <button onClick={() => navigate('/login', { replace: true })} width="100%" style={{width:"100%",fontSize:"20px", background:"white", color:"black",fontWeight:"600", borderColor:"lightgrey",borderRadius:"5px", margin:"0px 0px 10px 0px",boxShadow:"2px 3px 2px 2px lightgrey", padding:"10px 5px 10px 5px"}}>
-                Log in to an existing account
-              </button>
-              <ToastContainer />
-                </div>
+                <button onClick={() => navigate('/login', { replace: true })} className="loginbtnregis">Log in to an existing account</button>
+                <ToastContainer />
+              </div>
           </div>
-          
         </div>
-    
 
-        <div class="illustration" style={{height:"100%", width:"120%", background:"linear-gradient(to bottom, #820BC5, #531EB7)", padding:"30px 30px 0px 0px", overflow:"hidden"}} >
-            <h2 style={{color: 'white', fontSize:'300%', marginLeft:"12%" ,marginTop:'5%'}}>Welcome!</h2> 
-            <h2 style={{color: 'white', fontSize:'200%', marginLeft:"13%" ,marginTop:'-3%', fontWeight:"400"}}>Ready to get fit?</h2> 
-            <div style={{marginTop:"6%", marginLeft:"30px", marginRight:"auto",width:"100%", justifyContent:"center", alignItems:"center", display:"flex"}}>
+        {/*=======UI Decoration=========*/}
+        <div class="illustrationregis" >
+            <h2 className="illusregiswelcome">Welcome!</h2> 
+            <h2 className="illusregisready">Ready to get fit?</h2> 
+            <div className="illusimagediv">
               <img src={loginillustration} width={"650px"} ></img>
             </div>
-            <div style={{height:"70%", marginTop:"-45%",width:"105%",background:"linear-gradient(to bottom right, #8B01A1, #ED8500)", boxShadow:"6px 2px 4px 4px #700396",borderRadius:"500% 500% 0 0"}}></div>
+            <div className="regishalfcircledeco"></div>
         </div>
+
       </div>
     );
   };
